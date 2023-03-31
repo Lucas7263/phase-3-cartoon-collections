51968cc47502141eab7305190ed2d6260323b7bf
Lucas <lucaswoodyard@yahoo.com>
2023-03-30 21:43:33 -0500

    commit tree 1305cc6a6b9a79fd89fc171e4a7af9e35d0d8a77

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves dwarf # code an argument here
   # Your code here
+      dwarf.each.with_index(1) do |dwarf, i|
+        puts " #{i} #{dwarf}"
+      end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet summons # code an argument here
+  # Your code here 
+      
+  summons.map { |summon| summon.capitalize + "!"}
+    
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls  call # code an argument here
   # Your code here
+  
+    call.any? { |call| call.length > 4 }
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese potentially_cheesy_items # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
